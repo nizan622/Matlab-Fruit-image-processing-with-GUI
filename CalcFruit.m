74e5901e299ddd53da348228e77e26e5c4e94038
ShaiEli1991 <shaiel@ac.sce.ac.il>
2017-01-24 23:04:50 +0200

    Add files via upload

--- CalcFruit.m
+++ CalcFruit.m
@@ -1,29 +1,32 @@
 function sum = CalcFruit(imgPixel, MaskPixel, ImageNum)
-Temp = (MaskPixel * 100)/imgPixel;
-    switch ImageNum
-        case 2
-            if(Temp >= 85)
-                sum = true;
-            else
-                sum = false;
-            end
-        case 3
-            if(Temp >= 85)
-                sum = true;
-            else
-                sum = false;
-            end
-        case 4
-            if(Temp >= 75)
-                sum = true;
-            else
-                sum = false;
-            end
-        case 5
-            if(Temp >= 85)
-                sum = true;
-            else
-                sum = false;
-            end
-    end
+if ((imgPixel==0)||(MaskPixel==0)||(MaskPixel>imgPixel))
+    sum = false;
+else
+    Temp = (MaskPixel * 100)/imgPixel;
+        switch ImageNum
+            case 2
+                if(Temp >= 85)
+                    sum = true;
+                else
+                    sum = false;
+                end
+            case 3
+                if(Temp >= 85)
+                    sum = true;
+                else
+                    sum = false;
+                end
+            case 4
+                if(Temp >= 75)
+                    sum = true;
+                else
+                    sum = false;
+                end
+            case 5
+                if(Temp >= 85)
+                    sum = true;
+                else
+                    sum = false;
+                end
+        end
 end
