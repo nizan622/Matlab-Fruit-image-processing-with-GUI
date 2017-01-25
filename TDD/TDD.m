92ce7af4b4127f072bf85217a7cdff7d2bc2e776
nizan622 <nizanro@ac.sce.ac.il>
2017-01-25 23:23:06 +0200

    Update TDD.m

--- TDD/TDD.m
+++ TDD/TDD.m
@@ -4,32 +4,32 @@ classdef TDD < matlab.unittest.TestCase
         %Fruit funtion
         
 %         function testOne(testCase)
-%             [testOne, ~, ~, ~] = Fruit('Photos\avocado2.jpg',2);
+%             [testOne, ~, ~, ~] = TDD_Fruit('Photos\avocado2.jpg',2);
 %             testOneSol = ('redBand.jpg');
 %             testCase.verifyEqual(testOne,testOneSol);
 %         end
 %         function testOne(testCase)
-%             [testOne, ~, ~, ~] = Fruit('Photos\avocado2.jpg',2);
+%             [testOne, ~, ~, ~] = TDD_Fruit('Photos\avocado2.jpg',2);
 %             testOneSol = imread('redBand.jpg');
 %             testCase.verifyEqual(testOne,testOneSol);
 %         end
         function testOne(testCase)
-            [testOne, ~, ~, ~] = Fruit('Photos\avocado2.jpg',2);
+            [testOne, ~, ~, ~] = TDD_Fruit('Photos\avocado2.jpg',2);
             testOneSol = imread('redBand.jpg');
             testCase.verifyEqual(size(testOne),size(testOneSol));
         end
         function testTwo(testCase)
-            [~, testTwo, ~, ~] = Fruit('Photos/avocado2.jpg',2);
+            [~, testTwo, ~, ~] = TDD_Fruit('Photos/avocado2.jpg',2);
             testTwoSol = imread('blueBand.jpg');
             testCase.verifyEqual(size(testTwo),size(testTwoSol));
         end
         function testThree(testCase)
-            [~, ~, testThree, ~] = Fruit('Photos/avocado2.jpg',2);
+            [~, ~, testThree, ~] = TDD_Fruit('Photos/avocado2.jpg',2);
             testThreeSol = imread('greenBand.jpg');
             testCase.verifyEqual(size(testThree),size(testThreeSol));
         end
         function testFour(testCase)
-            [~, ~, ~, testFour] = Fruit('Photos/avocado2.jpg',2);
+            [~, ~, ~, testFour] = TDD_Fruit('Photos/avocado2.jpg',2);
             testFourSol = imread('maskedrgbImage.jpg');
             testCase.verifyEqual(size(testFour),size(testFourSol));
         end
@@ -37,22 +37,22 @@ classdef TDD < matlab.unittest.TestCase
         
         %Diameter Function:
         function testOneDiameter(testCase)
-            [testOne,~,~] = Diameter('Photos\avocado2.jpg');
+            [testOne,~,~] = TDD_Diameter('Photos\avocado2.jpg');
             testOneSol = imread('blue.jpg');
             testCase.verifyEqual(size(testOne),size(testOneSol))
         end
         function testTwoDiameter(testCase)
-            [~,testTwo,~] = Diameter('Photos\avocado2.jpg');
+            [~,testTwo,~] = TDD_Diameter('Photos\avocado2.jpg');
             testTwoSol = imread('bw.jpg');
             testCase.verifyEqual(size(testTwo),size(testTwoSol))
         end
 %         function testThreeDiameter(testCase)
-%             [~,~,testThree] = Diameter('Photos\avocado2.jpg');
+%             [~,~,testThree] = TDD_Diameter('Photos\avocado2.jpg');
 %             testThreeSol = 1.5;
 %             testCase.verifyEqual(testThree,testThreeSol)
 %         end
         function testThreeDiameter(testCase)
-            [~,~,testThree] = Diameter('Photos\avocado2.jpg');
+            [~,~,testThree] = TDD_Diameter('Photos\avocado2.jpg');
             testThreeSol = 1.5;
             testCase.verifyEqual(testThree,testThreeSol,'RelTol', 0.1)
         end
